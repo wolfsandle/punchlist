c7ea046efa312c6496f87887e823f9b2804160c9
Prelang Builder <builder@prelang.com>
2016-08-05 03:15:46 +0000

    Setting the User of the model on #create to the currently signed in user

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -29,6 +29,7 @@ class RoomsController < ApplicationController
   # POST /rooms.json
   def create
     @room = Room.new(room_params)
+    @room.user = current_user
 
     respond_to do |format|
       if @room.save
